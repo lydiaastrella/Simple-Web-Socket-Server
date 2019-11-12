159f71ae1865a59a9381aecdc57980fea5cf03a3
Lydia Astrella <lydiaastrella@gmail.com>
2019-11-12 12:55:59 +0700

    add function handshake

--- server.py
+++ server.py
@@ -2,6 +2,17 @@ import socket
 import base64
 import hashlib
 
+def handshake(key):
+    key = key + MAGIC_STRING
+    key = key.encode()
+
+    responseKey = base64.b64encode(hashlib.sha1(key).digest()).decode()
+    response =  "HTTP/1.1 101 Switching Protocols\r\n" + \
+                "Upgrade: websocket\r\n" + \
+                "Connection: Upgrade\r\n" + \
+                "Sec-WebSocket-Accept: %s\r\n\r\n"%(responseKey)
+    return response.encode()
+
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
 MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" #string to concatenate to key
@@ -22,13 +33,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 for x in headers :
                     if "Sec-WebSocket-Key:" in x:
                         key = x.split(" ")[1]
-                        key = key + MAGIC_STRING
-                        key = key.encode()
-                
-                responseKey = base64.b64encode(hashlib.sha1(key).digest()).decode()
-                response = "HTTP/1.1 101 Switching Protocols\r\n" + \
-                            "Upgrade: websocket\r\n" + \
-                            "Connection: Upgrade\r\n" + \
-                            "Sec-WebSocket-Accept: %s\r\n\r\n"%(responseKey)
-                response = response.encode()
+                        break
+                response = handshake(key)
                 conn.sendall(response)
+            else:
+                #send bad request message
+                print("bad request message, belum dibikin")
